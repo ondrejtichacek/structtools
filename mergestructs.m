c1a40f3c1c4bf511ea3fc27f12ad169c90cee53a
sebolains <sebmaurino@gmail.com>
2020-09-04 16:05:21 -0400

    Added initial helper functions

---
+++ mergestructs.m
@@ -0,0 +1,39 @@
+function S = mergestructs(S, T)
+%MERGESTRUCTS Combines fields of both input structures into a single structure
+%   Output structure will contain all fields and their corresponding values of
+%   the two input structures. If a field is present in both input structures,
+%   the field in S is used, and a warning is produced. This warning can be
+%   turned off using warning('off', "structtools:commonfield")
+%
+%   Example:
+%   S.one = 1;
+%   T.two = 2;
+%   mergestructs(S,T)
+%
+%   ans = 
+%   struct with fields:
+%     one: 1
+%     two: 2
+%
+%   Inputs:
+%   S       first structure to merge
+%   T       second structure to merge
+%
+%   Output:
+%   S       combination structure
+
+    arguments
+        S(1,1) struct
+        T(1,1) struct
+    end
+
+    f = fieldnames(T);
+    for i = 1:length(f)
+        if isfield(S, f{i})
+            warning("structtools:commonfield",...
+                "Field '%s' exists in both structures.", f{i});
+            continue
+        end
+        S.(f{i}) = T.(f{i});
+    end
+end
